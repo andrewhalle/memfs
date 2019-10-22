1283f7568b1cc2b672909d995bf60f212277a28e
Andrew Halle <andrew@livelyme.com>
2019-10-21 17:24:43 -0700

    add Directory and Filesystem structs to implement later, and utility structs for holding permission/ownership data, add a method on Directory for creating empty files

--- memfs/src/file.rs
+++ memfs/src/file.rs
@@ -1,5 +1,38 @@
+use std::collections::HashMap;
+
+#[derive(Default)]
+pub struct RWEPerm {
+    read: bool,
+    write: bool,
+    exec: bool,
+}
+
+#[derive(Default)]
+pub struct Permissions {
+    owner: RWEPerm,
+    group: RWEPerm,
+    everyone: RWEPerm,
+}
+
+#[derive(Default)]
+pub struct Ownership {
+    owner: usize,
+    group: usize
+}
+
 pub struct File {
     data: Vec<u8>,
+    owner: Ownership,
+    permissions: Permissions,
+}
+
+pub struct Directory {
+    files: HashMap<String, File>,
+    directories: HashMap<String, Directory>,
+}
+
+pub struct Filesystem {
+    root: Directory
 }
 
 impl File {
@@ -10,8 +43,34 @@ impl File {
         }
 
         File {
-            data
+            data,
+            owner: Ownership::default(),
+            permissions: Permissions::default(),
         }
+    }
+
+    pub fn empty() -> File {
+        File {
+            data: Vec::new(),
+            owner: Ownership::default(),
+            permissions: Permissions::default(),
+        }
+    }
+}
+
+impl Directory {
+    pub fn new() -> Directory {
+        Directory { files: HashMap::new(), directories: HashMap::new() }
+    }
+
+    pub fn touch(&mut self, filename: &str) {
+        self.files.insert(filename.to_string(), File::empty());
+    }
+}
+
+impl Filesystem {
+    pub fn new() -> Filesystem {
+        Filesystem { root: Directory::new() }
     }
 }
 
